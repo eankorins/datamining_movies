8867b87dd15e579cd045a3e9ffbb0823db2d2328
Edgars Ankorins <eankorins@gmail.com>
2015-04-08 14:02:14 +0200

    MODELSgit add .!

--- db/migrate/20150406130627_create_movie.rb
+++ db/migrate/20150406130627_create_movie.rb
@@ -2,6 +2,24 @@ class CreateMovie < ActiveRecord::Migration
   def change
     create_table :movies do |t|
       t.string :title
+      t.integer :director_id
+      t.integer :length
+      t.string :trailer_url
+      t.string :mpaa_rating
+      t.decimal :imdb_rating
+      t.datetime :release_date
+      t.integer :imdb_votes
+      t.integer :release_year
+      t.bool :adult
+      t.string :backdrop_path
+      t.integer :budget
+      t.string :overview
+      t.decimal :tmdb_popularity
+      t.string :poster_path
+      t.integer :revenue
+      t.string :tagline
+      t.decimal :tmdb_vote_average
+      t.integer :tmdb_vote_count
       t.integer :imdb_id
       t.integer :tmdb_id
       t.timestamps null: false
